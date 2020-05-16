312830bafa653e7c5d850e76f4565be0e5cac9c8
king-11 <lakshay.singh1108@gmail.com>
2020-05-16 22:55:07 +0530

    Repo structure maintained

--- Implementation/Tabs with icon and svg/javascript/master.js
+++
@@ -1,7 +0,0 @@
-let swapItems = document.querySelectorAll('.swp');
-swapItems.forEach((item, i) => {
-  item.addEventListener('click',() => {
-    document.querySelector('.swp-active').classList.remove("swp-active");
-    item.classList.add("swp-active");
-  })
-});
