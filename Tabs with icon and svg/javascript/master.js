0af052765f58faf8660edc849d6f18502c19604c
Atul-tyagi23 <63414418+Atul-tyagi23@users.noreply.github.com>
2020-05-20 14:35:20 +0530

    Wev-design

--- Tabs with icon and svg/javascript/master.js
+++ Tabs with icon and svg/javascript/master.js
@@ -1,7 +1,24 @@
+
 let swapItems = document.querySelectorAll('.swp');
 swapItems.forEach((item, i) => {
   item.addEventListener('click',() => {
     document.querySelector('.swp-active').classList.remove("swp-active");
     item.classList.add("swp-active");
-  })
+  });
+});
+
+let ham = document.querySelector('.hamBtn');
+let nav = document.querySelector('.swapper');
+let link = document.querySelector('.swapper-link');
+
+
+
+ham.addEventListener('click', () => {
+     nav.classList.toggle('swapper2');
+     console.log(window.innerWidth);
+
+});
+
+window.addEventListener("resize", function() {
+  if (window.innerWidth < 700) nav.classList.remove("swapper2");
 });
